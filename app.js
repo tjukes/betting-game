6978d63461d69583b01db0006c7ec6b46058f5b7
Tana Jukes <tana.jukes@gmail.com>
2016-06-28 14:27:26 -0700

    Change bettingGame (object) into constructor function that can be called to instantiate new games
    Previously, there was no logic present to allow the player to reset the game and start again. Converting the initial bettingGame object into a constructor function, which is called once when the page loads, will allow for new games to start.
    
    The new constructor function also takes an argument, which is assigned to be the player's starting balance variable. This will allow for future games to be started with different initial amounts.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
 
-  var bettingGame = {
+  function bettingGame(starting_balance) {
     // Initialize player's bank balance
-    playerBankroll: 100,
+    this.playerBankroll = starting_balance,
 
-    playRound: function(bet, guess) {
+    this.playRound = function(bet, guess) {
       // Initialize new random number between 1 and 10 to guess
       var guessMe = Math.ceil(Math.random() * 10);
       // Check answer and update
@@ -26,10 +26,12 @@ $(document).ready(function() {
     }
   };
 
+  var newGame = new bettingGame(100);
+
   $("button#play-round").on("click", function() {
     var playerBet = $("select#bet").val();
     var playerGuess = $("input#guess").val();
-    bettingGame.playRound(playerBet, playerGuess);
+    newGame.playRound(playerBet, playerGuess);
   });
 
 });
