065333c72b81f93285ba975531f16ddb6fe9abcf
Tana Jukes <tana.jukes@gmail.com>
2016-06-28 09:39:51 -0700

    jQuery-ify game functionality
    Move all game functionality into jQuery and improve it:
    - 'bettingGame' object moved into .ready call
    - Make game output new balance to a span element in the view instead of just displaying in an alert
    - New method call attached to click event on 'Play Game' button:
    -- Get values from form fields for player's guess and bet amount and assign them to local variables
    -- Pass those variables into a call to bettingGame.playRound() to initiate a new round

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 
-var bettingGame = {
-  // Initialize player's bank balance
-  playerBankroll: 100,
+$(document).ready(function() {
 
-  playRound: function(bet, guess) {
-    // Initialize new random number between 1 and 10 to guess
-    var guessMe = Math.ceil(Math.random() * 10);
-    // Check answer and update
-    switch (guess) {
-      case (guessMe + 1 || guessMe - 1):
-        alert("Close!");
-        break;
-      case guessMe:
-        this.playerBankroll += bet;
-        alert("Good guess - your new balance is " + this.playerBankroll);
-        break;
-      default:
-        this.playerBankroll -= bet;
-        alert("Better luck next time... your new balance is " + this.playerBankroll);
+  var bettingGame = {
+    // Initialize player's bank balance
+    playerBankroll: 100,
+
+    playRound: function(bet, guess) {
+      // Initialize new random number between 1 and 10 to guess
+      var guessMe = Math.ceil(Math.random() * 10);
+      // Check answer and update
+      switch (guess) {
+        case (guessMe + 1 || guessMe - 1):
+          alert("Close!");
+          break;
+        case guessMe:
+          this.playerBankroll += bet;
+          alert("Good guess - your new balance is " + this.playerBankroll);
+          $("span#current-balance").text(this.playerBankroll);
+          break;
+        default:
+          this.playerBankroll -= bet;
+          alert("Better luck next time... your new balance is " + this.playerBankroll);
+          $("span#current-balance").text(this.playerBankroll);
+      }
     }
-  }
-};
+  };
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("play-round").addEventListener("click", function() {
-    var playerBet = prompt("What's your bet? ($5 - $10)");
-    var playerGuess = prompt("What's your guess? (1-10)");
+  $("button#play-round").on("click", function() {
+    var playerBet = $("select#bet").val();
+    var playerGuess = $("input#guess").val();
     bettingGame.playRound(playerBet, playerGuess);
   });
+
 });
